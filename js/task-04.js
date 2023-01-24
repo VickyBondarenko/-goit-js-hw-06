79f8086d97ad4f55d1a79ff4a97c34154ab25a80
VickyBondarenko <quikyv@gmail.com>
2023-01-24 12:54:00 +0100

    Change-task4

--- js/task-04.js
+++ js/task-04.js
@@ -6,12 +6,12 @@ const decButton = document.querySelector('button[data-action="decrement"]');
 const incrButton = document.querySelector('button[data-action="increment"]');
 
 decButton.addEventListener("click", (event) => {
-  value += 1;
-  console.log(value);
+  value -= 1;
+
   return (counterValue.textContent = value);
 });
 incrButton.addEventListener("click", (event) => {
-  value -= 1;
-  console.log(value);
+  value += 1;
+
   return (counterValue.textContent = value);
 });
--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ const nameOutput = document.querySelector("#name-output");
 
 nameIntput.addEventListener("input", (event) => {
   nameOutput.textContent = event.currentTarget.value;
-  if (!event.currentTarget.value) {
+  if (!event.currentTarget.value || event.currentTarget.value === " ") {
     nameOutput.textContent = `Anonymous`;
   }
 });
