a8f58e4770f35727ab76f1930ca6116c35725707
VickyBondarenko <quikyv@gmail.com>
2023-01-24 13:18:00 +0100

    Change-task5

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,8 @@ const nameIntput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameIntput.addEventListener("input", (event) => {
-  nameOutput.textContent = event.currentTarget.value;
-  if (!event.currentTarget.value || event.currentTarget.value === " ") {
-    nameOutput.textContent = `Anonymous`;
+  if (event.currentTarget.value === "") {
+    return (nameOutput.textContent = `Anonymous`);
   }
+  nameOutput.textContent = event.currentTarget.value;
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,12 @@
 const textInput = document.querySelector("#validation-input");
 
-textInput.addEventListener("blur", () => {
-  console.log(textInput.data - lenght);
+console.log(textInput);
+console.log(textInput.id);
+console.log(textInput.type);
+console.log(textInput.data - length);
+console.log(textInput.placeholder);
+
+textInput.addEventListener("blur", (event) => {
   if (textInput.value.length !== 6) {
     textInput.classList.add("invalid");
     textInput.classList.remove("valid");
